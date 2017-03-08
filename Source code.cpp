77948d6253a7714f2fa4e3b1b3cb05af58a20cc7
oeplxj6h <thdi@ro.ru>
2017-03-08 18:28:09 +0000

    Rename the variable "position" to more correct "start_position"
    Soon I will add the new variable called "position"

--- Source code.cpp
+++ Source code.cpp
@@ -6,14 +6,14 @@ using namespace std;
 void main(){
 	setlocale(LC_ALL, "rus");
 	bool nonconformity;
-	unsigned short int position,cycle;
+	unsigned short int start_position,cycle;
 	signed int mood=0;
 	char string[1024];
 	char words[16][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","глуп","туп"};
 	ifstream stream("TEXT.TXT");
 	while(!stream.eof()){
 		stream>>string;
-		position=0;
+		start_position=0;
 	}
 	stream.close();
 }
