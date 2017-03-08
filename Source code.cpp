3dd4e85f712b19f9f6e6aa93c32364daf90d6bfa
oeplxj6h <thdi@ro.ru>
2017-03-08 18:58:52 +0000

    Added 2 new variants for negative words' beginnings

--- Source code.cpp
+++ Source code.cpp
@@ -10,14 +10,14 @@ void main(){
 	unsigned short int start_position,position,cycle;
 	signed int mood=0;
 	char string[1024];
-	char words[16][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","глуп","туп"};
+	char words[18][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","хуж","худш","глуп","туп"};
 	ifstream stream("TEXT.TXT");
 	while(!stream.eof()){
 		stream>>string;
 		start_position=0;
 		do{
 			conformity=false;
-			for(cycle=0;cycle<16;++cycle){
+			for(cycle=0;cycle<18;++cycle){
 				if(!conformity){
 
 				}
