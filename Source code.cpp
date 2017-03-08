ddec7365a09b20833ab582aba1e7a35cd3a129a5
oeplxj6h <thdi@ro.ru>
2017-03-08 17:19:27 +0000

    Added special variable for cycles

--- Source code.cpp
+++ Source code.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void main(){
 	setlocale(LC_ALL, "rus");
 	bool nonconformity;
-	unsigned short int position;
+	unsigned short int position,cycle;
 	signed int mood=0;
 	char string[1024];
 	char words[16][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","глуп","туп"};
