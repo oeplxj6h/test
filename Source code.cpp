59581c3b2268cf0e9d0645246118fde230c7250c
oeplxj6h <thdi@ro.ru>
2017-03-08 16:43:41 +0000

    Added special variable to store the index of mood

--- Source code.cpp
+++ Source code.cpp
@@ -7,6 +7,7 @@ void main(){
 	setlocale(LC_ALL, "rus");
 	bool nonconformity;
 	unsigned short int position;
+	signed int mood=0;
 	char string[1024];
 	char words[16][23]={"плох","нехорош","некачествен","пошл","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негати","глуп","туп"};
 	ifstream stream("TEXT.TXT");
