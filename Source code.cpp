a4ee384b97f1d8bff201a6d83b093e364135d680
oeplxj6h <thdi@ro.ru>
2017-03-08 16:28:33 +0000

    Added service variables
    Added service variables: one for cycles and a flag to fixate nonconformity of strings.

--- Source code.cpp
+++ Source code.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 void main(){
 	setlocale(LC_ALL, "rus");
+	bool nonconformity;
+	unsigned short int position;
 	char string[1024];
 	char words[16][23]={"плох","нехорош","некачествен","пошл","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негати","глуп","туп"};
 	ifstream stream("TEXT.TXT");
