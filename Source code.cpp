d75d83cb09a68bbeb1f9f1ff0632db0667656c14
oeplxj6h <thdi@ro.ru>
2017-03-09 18:13:20 +0300

    Added feature to recoginze non-beginning parts of words and corrected examples

--- Source code.cpp
+++ Source code.cpp
@@ -12,30 +12,32 @@ char make_letter_lower_case(char letter){
 
 void main(){
 	setlocale(LC_ALL, "rus");
-	bool conformity;
+	bool conformity,service_variable;
 	unsigned short int start_position,position,cycle;
 	signed int mood=0;
 	char string[1024];
-	char words[18][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","хуж","худш","глуп","туп"};
+	char words[19][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","хуж","худ","мут","глу","туп"};
 	ifstream stream("TEXT.TXT");
 	while(!stream.eof()){
 		stream>>string;
 		start_position=0;
 		do{
 			conformity=false;
-			for(cycle=0;cycle<18;++cycle)
-				if(!conformity){
-					for(position=start_position;make_letter_lower_case(string[position])==words[cycle][position-start_position];++position);
-					if(position-start_position==strlen(words[cycle]))
-						conformity=true;
-				}
+			while(string[start_position]!=' '){
+				for(cycle=0;cycle<19;++cycle)
+					if(!conformity){
+						for(position=start_position;make_letter_lower_case(string[position])==words[cycle][position-start_position];++position);
+						if(position-start_position==strlen(words[cycle]))
+							conformity=true;
+					}
+				++start_position;
+			}
 			if(conformity)
 				++mood;
-			start_position=position;
-			while(string[++start_position]!=' ');
 			while(string[++start_position]==' ');
 		}while(start_position<strlen(string));
 	}
 	stream.close();
 	cout<<"Количество негативных слов в тексте, заданном файлом TEXT.TXT: "<<mood<<endl<<endl;
+	cin>>service_variable;
 }
