9d7a5a9b0b13e3a689da7b1a9b546004f3ab6be8
oeplxj6h <thdi@ro.ru>
2017-03-15 17:25:55 +0300

    Deleted variable "conformity" and added percentage calculation

--- Source code.cpp
+++ Source code.cpp
@@ -12,32 +12,30 @@ char make_letter_lower_case(char letter){
 
 void main(){
 	setlocale(LC_ALL, "rus");
-	bool conformity,service_variable;
+	bool service_variable;
 	unsigned short int start_position,position,cycle;
-	signed int mood=0;
+	unsigned int mood=0,number_of_words=0;
 	char string[1024];
-	char words[19][13]={"плох","нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","хруп","скот","гад","отриц","негат","хуж","худ","мут","глу","туп"};
+	char words[18][13]={"нехоро","некач","пошло","дур","дрян","горе","горь","тёмн","тьм","хруп","скот","гад","отриц","негат","хуж","мут","глу","туп"};
 	ifstream stream("TEXT.TXT");
 	while(!stream.eof()){
 		stream>>string;
 		start_position=0;
 		do{
-			conformity=false;
 			while(string[start_position]!=' '){
-				for(cycle=0;cycle<19;++cycle)
-					if(!conformity){
-						for(position=start_position;make_letter_lower_case(string[position])==words[cycle][position-start_position];++position);
-						if(position-start_position==strlen(words[cycle]))
-							conformity=true;
-					}
+				for(cycle=0;cycle<18;++cycle){
+					for(position=start_position;make_letter_lower_case(string[position])==words[cycle][position-start_position];++position);
+					if(position-start_position==strlen(words[cycle]))
+						++mood;
+				}
 				++start_position;
 			}
-			if(conformity)
-				++mood;
 			while(string[++start_position]==' ');
+			++number_of_words;
 		}while(start_position<strlen(string));
 	}
 	stream.close();
 	cout<<"Количество негативных слов в тексте, заданном файлом TEXT.TXT: "<<mood<<endl<<endl;
+	cout<<"Отношение количества негативных слов к их общему количеству: "<<(double)mood/(double)number_of_words<<endl<<endl;
 	cin>>service_variable;
 }
